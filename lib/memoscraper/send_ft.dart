bd9d5a6f2a4d3228de97c20fdb66d4f04b7e8092
The Real Bitcoin Club <therealbitcoin.club@protonmail.com>
2025-08-20 12:25:40 -0400

    send FT example

--- lib/memoscraper/send_ft.dart
+++ lib/memoscraper/send_ft.dart
@@ -1,35 +1,140 @@
 import 'package:bitcoin_base/bitcoin_base.dart';
 import 'package:blockchain_utils/blockchain_utils.dart';
-import 'package:instagram_clone1/memoscraper/socket/electrum_websocket_service.dart';
+import 'socket/electrum_websocket_service.dart';
 
 void main() async {
-  final service = await ElectrumWebSocketService.connect(
+  ElectrumWebSocketService service = await ElectrumWebSocketService.connect(
       "wss://bch.imaginary.cash:50004");
 
-  final provider = ElectrumProvider(service);
-  final List<int> senderSeed = Bip39SeedGenerator(Mnemonic.fromString(
-      "xxx-xxx-xxx..."))
-      .generate();
+  String tokenId = "your-FT-token-id";
+  BitcoinCashNetwork network = BitcoinCashNetwork.mainnet;
 
-  final Bip32Slip10Secp256k1 bip32Sender = Bip32Slip10Secp256k1.fromSeed(senderSeed);
-  final bip44Sender = bip32Sender.derivePath("m/44'/145'/0'/0/0");
-
-  final ECPrivate privateKeySender = ECPrivate.fromBytes(bip44Sender.privateKey.raw);
-
-  final ECPublic publicKeySender = privateKeySender.getPublic();
-
-  final P2pkhAddress senderP2PKHWT = P2pkhAddress.fromHash160(
-      addrHash: publicKeySender.toAddress().addressProgram,
-      type: P2pkhAddressType.p2pkhwt);
-
-  final electrumUTXOs = await provider.request(ElectrumRequestScriptHashListUnspent(
+  ElectrumProvider provider = ElectrumProvider(service);
+  ECPrivate bip44Sender = createBip44PrivateKey(
+      "12-word-mnemonic that you can obtain on cashonize.com");
+  P2pkhAddress senderP2PKHWT = createAddressP2PKHWT(bip44Sender);
+  ECPrivate bip44Receiver = createBip44PrivateKey(
+      "12-word-mnemonic that you can obtain on cashonize.com");
+  P2pkhAddress receiverP2PKHWT = createAddressP2PKHWT(bip44Receiver);
+  
+  BitcoinCashAddress senderBCHp2pkhwt = BitcoinCashAddress.fromBaseAddress(senderP2PKHWT);
+  
+  List<ElectrumUtxo> electrumUTXOs = await provider.request(ElectrumRequestScriptHashListUnspent(
     scriptHash: senderP2PKHWT.pubKeyHash(),
     includeTokens: true,
   ));
 
   if (electrumUTXOs.length == 0) {
     print("Zero UTXOs found");
-  } else {
-    print("Successfully imported a valid seed phrase with UTXOs");
+    return;
   }
+
+  List<UtxoWithAddress> utxos = transformUtxosFilterForTokenId(electrumUTXOs, senderBCHp2pkhwt, bip44Sender, tokenId);
+
+  BigInt totalAmountInSatoshisAvailable = utxos.sumOfUtxosValue();
+  if (totalAmountInSatoshisAvailable == BigInt.zero) {
+    print("Zero UTXOs with that tokenId found");
+    return;
+  }
+
+  CashToken token = findTokenById(electrumUTXOs, tokenId);
+  BigInt totalAmountOfTokenAvailable = calculateTotalAmountOfThatToken(utxos, tokenId);
+
+  ForkedTransactionBuilder bchTransaction = buildTxToTransferTokens(
+      15,
+      senderBCHp2pkhwt,
+      totalAmountInSatoshisAvailable,
+      utxos,
+      receiverP2PKHWT,
+      token,
+      totalAmountOfTokenAvailable,
+      network);
+
+  BtcTransaction signedTx = bchTransaction.buildTransaction((trDigest, utxo, publicKey, sighash) {
+    return bip44Sender.signECDSA(trDigest, sighash: sighash);
+  });
+
+  await provider.request(
+      ElectrumRequestBroadCastTransaction(transactionRaw: signedTx.toHex()));
+  print("success");
+}
+
+ForkedTransactionBuilder buildTxToTransferTokens(int tokenAmountToSend, BitcoinCashAddress senderBCHp2pkhwt, BigInt totalAmountInSatoshisAvailable, List<UtxoWithAddress> utxos, P2pkhAddress receiverP2PKHWT, CashToken token, BigInt totalAmountOfTokenAvailable, BitcoinCashNetwork network) {
+  var amount = BigInt.from(tokenAmountToSend);
+  BigInt fee = BtcUtils.toSatoshi("0.00003");
+  return ForkedTransactionBuilder(
+    outPuts: [
+      /// change address for bch values (sum of bch amout - (outputs amount + fee))
+      BitcoinOutput(
+        address: senderBCHp2pkhwt.baseAddress,
+        value: totalAmountInSatoshisAvailable -
+            (BtcUtils.toSatoshi("0.00002") + fee),
+      ),
+      BitcoinTokenOutput(
+          utxoHash: utxos.first.utxo.txHash,
+          address: receiverP2PKHWT,
+          value: BtcUtils.toSatoshi("0.00001"),
+          token: token.copyWith(amount: amount)),
+      BitcoinTokenOutput(
+          utxoHash: utxos.first.utxo.txHash,
+          address: senderBCHp2pkhwt.baseAddress,
+          value: BtcUtils.toSatoshi("0.00001"),
+          token: token.copyWith(amount: totalAmountOfTokenAvailable - amount)),
+    ],
+    fee: fee,
+    network: network,
+    memo: null,
+    utxos: utxos,
+  );
+}
+
+CashToken findTokenById(List<ElectrumUtxo> electrumUTXOs, String tokenId) {
+  return electrumUTXOs
+      .firstWhere((e) =>
+  e.token?.category ==
+      tokenId)
+      .token!;
+}
+
+BigInt calculateTotalAmountOfThatToken(List<UtxoWithAddress> utxos, String tokenId) {
+  return utxos
+      .where((element) =>
+  element.utxo.token?.category ==
+      tokenId)
+      .fold(
+      BigInt.zero,
+          (previousValue, element) =>
+      previousValue + element.utxo.token!.amount);
+}
+
+List<UtxoWithAddress> transformUtxosFilterForTokenId(List<ElectrumUtxo> electrumUTXOs, BitcoinCashAddress senderBCHp2pkhwt, ECPrivate bip44Sender, String tokenId) {
+  return electrumUTXOs
+      .map((e) => UtxoWithAddress(
+      utxo: e.toUtxo(senderBCHp2pkhwt.type),
+      ownerDetails: UtxoAddressDetails(
+          publicKey: bip44Sender.getPublic().toHex(), address: senderBCHp2pkhwt.baseAddress)))
+      .toList()
+      .where((element) {
+    return element.utxo.token?.category ==
+        tokenId ||
+        element.utxo.token == null;
+  }).toList();
+}
+
+P2pkhAddress createAddressP2PKHWT(ECPrivate pk) {
+  ECPublic pubKey = pk.getPublic();
+  
+  return P2pkhAddress.fromHash160(
+      addrHash: pubKey.toAddress().addressProgram,
+      type: P2pkhAddressType.p2pkhwt);
+}
+
+ECPrivate createBip44PrivateKey(String mnemonic) {
+  List<int> seed = Bip39SeedGenerator(Mnemonic.fromString(
+      mnemonic))
+      .generate();
+  
+  Bip32Slip10Secp256k1 bip32 = Bip32Slip10Secp256k1.fromSeed(seed);
+  Bip32Base bip44 = bip32.derivePath("m/44'/145'/0'/0/0");
+  return ECPrivate.fromBytes(bip44.privateKey.raw);
 }

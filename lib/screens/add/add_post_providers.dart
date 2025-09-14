7bb423603a0c2b1a4ba0b3a219b6cbe6c18bcedc
The Real Bitcoin Club <therealbitcoin.club@protonmail.com>
2025-09-14 11:44:32 -0400

    temp tip

--- lib/screens/add/add_post_providers.dart
+++ lib/screens/add/add_post_providers.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../memo/model/memo_model_user.dart';
+
 // State providers for each media type
 final imgurUrlProvider = StateProvider<String>((ref) => '');
 final youtubeVideoIdProvider = StateProvider<String>((ref) => '');
@@ -14,3 +16,5 @@ final isPublishingProvider = StateProvider<bool>((ref) => false);
 
 // Provider for tag controller text
 final tagTextProvider = StateProvider<String>((ref) => '');
+
+final temporaryTipAmountProvider = StateProvider<TipAmount?>((ref) => null);

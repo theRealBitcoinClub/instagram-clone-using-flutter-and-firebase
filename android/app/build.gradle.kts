6e8ca41ca4f3406ad559b404c1b3a8ab01f70cf6
The Real Bitcoin Club <therealbitcoin.club@protonmail.com>
2025-09-16 20:06:29 -0400

    asdf

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,7 +29,7 @@ val flutterCompileSdkVersion = 36
 val flutterMinSdkVersion = 27
 val flutterTargetSdkVersion = 36
 val flutterNdkVersion = "27.0.12077973"
-val flutterVersionCode = 2025091513
+val flutterVersionCode = 2025091514
 val flutterVersionName = "2.7.2-BCH"
 
 android {
@@ -76,7 +76,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutterMinSdkVersion
         targetSdk = flutterTargetSdkVersion
-        versionCode = 2025091513
+        versionCode = 2025091514
         versionName = "2.7.2-BCH"
     }
 

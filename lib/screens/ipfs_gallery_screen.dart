777632a6fb12988ea8979b8e7e2949cd71c6807d
The Real Bitcoin Club <therealbitcoin.club@protonmail.com>
2025-09-20 18:26:26 -0400

    Update ipfs_gallery_screen.dart

--- lib/screens/ipfs_gallery_screen.dart
+++ lib/screens/ipfs_gallery_screen.dart
@@ -5,6 +5,7 @@ import 'package:mahakka/config_ipfs.dart';
 import 'package:mahakka/screens/ipfs_pin_claim_screen.dart';
 import 'package:share_plus/share_plus.dart';
 
+import '../widgets/animations/animated_grow_fade_in.dart';
 import 'add/add_post_providers.dart';
 
 class IPFSGalleryScreen extends ConsumerStatefulWidget {
@@ -283,79 +284,5 @@ class _IPFSGalleryScreenState extends ConsumerState<IPFSGalleryScreen> {
 
     ref.read(ipfsCidProvider.notifier).state = _selectedCid!;
     Navigator.pop(context);
-  }
-}
-
-// AnimatedGrowFadeIn widget (as provided)
-class AnimatedGrowFadeIn extends StatefulWidget {
-  final Widget child;
-  final bool show;
-  final Duration duration;
-  final Duration delay;
-  final Curve sizeCurve;
-  final Curve fadeCurve;
-  final AlignmentGeometry alignment;
-
-  const AnimatedGrowFadeIn({
-    Key? key,
-    required this.child,
-    required this.show,
-    this.duration = const Duration(milliseconds: 300),
-    this.delay = Duration.zero,
-    this.sizeCurve = Curves.fastOutSlowIn,
-    this.fadeCurve = Curves.easeIn,
-    this.alignment = Alignment.topCenter,
-  }) : super(key: key);
-
-  @override
-  State<AnimatedGrowFadeIn> createState() => _AnimatedGrowFadeInState();
-}
-
-class _AnimatedGrowFadeInState extends State<AnimatedGrowFadeIn> with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation<double> _fadeAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-    _animationController = AnimationController(vsync: this, duration: widget.duration, value: widget.show ? 1.0 : 0.0);
-    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(CurvedAnimation(parent: _animationController, curve: widget.fadeCurve));
-  }
-
-  @override
-  void didUpdateWidget(AnimatedGrowFadeIn oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (widget.show != oldWidget.show) {
-      if (widget.show) {
-        if (widget.delay == Duration.zero) {
-          _animationController.forward();
-        } else {
-          Future.delayed(widget.delay, () {
-            if (mounted) _animationController.forward();
-          });
-        }
-      } else {
-        _animationController.reverse();
-      }
-    }
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return FadeTransition(
-      opacity: _fadeAnimation,
-      child: AnimatedSize(
-        duration: widget.duration,
-        curve: widget.sizeCurve,
-        alignment: widget.alignment,
-        child: widget.show ? widget.child : Container(width: double.infinity),
-      ),
-    );
   }
 }

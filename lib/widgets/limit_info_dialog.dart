c9155f5e33fbf4bc06ceb16d4b5d88f47155ce73
The Real Bitcoin Club <therealbitcoin.club@protonmail.com>
2025-10-05 17:02:26 -0400

    padding

--- lib/widgets/limit_info_dialog.dart
+++ lib/widgets/limit_info_dialog.dart
@@ -1,7 +1,6 @@
 // lib/widgets/limit_info_dialog.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:mahakka/providers/token_limits_provider.dart';
 import 'package:mahakka/widgets/limit_info_widget.dart';
 
 class LimitInfoDialog extends ConsumerWidget {
@@ -19,36 +18,16 @@ class LimitInfoDialog extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final theme = Theme.of(context);
-    final tokenEnum = ref.watch(currentTokenLimitEnumProvider);
-
     return Dialog(
+      insetPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 3),
       backgroundColor: theme.colorScheme.surface,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
       child: Padding(
-        padding: const EdgeInsets.all(12),
+        padding: const EdgeInsets.all(3),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Row(
-            //   children: [
-            //     Icon(Icons.workspace_premium, color: theme.colorScheme.secondary, size: 28),
-            //     const SizedBox(width: 12),
-            //     Expanded(
-            //       child: Text(
-            //         '${tokenEnum.name} Plan Limits',
-            //         style: theme.textTheme.headlineSmall?.copyWith(color: theme.colorScheme.onSurface),
-            //       ),
-            //     ),
-            //     IconButton(
-            //       icon: Icon(Icons.close, color: theme.colorScheme.onSurface.withOpacity(0.6)),
-            //       onPressed: () => Navigator.of(context).pop(),
-            //     ),
-            //   ],
-            // ),
-            // const SizedBox(height: 16),
-            LimitInfoWidget(limitType: limitType, compact: false),
-          ],
+          children: [LimitInfoWidget(limitType: limitType, compact: false)],
         ),
       ),
     );

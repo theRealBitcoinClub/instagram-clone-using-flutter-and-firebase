0e5cd30ce3714caf7d5527019c91affbdcb25792
The Real Bitcoin Club <therealbitcoin.club@protonmail.com>
2025-10-09 11:56:27 -0400

    4.2.42-BCH+2025101001

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,8 +29,8 @@ val flutterCompileSdkVersion = 36
 val flutterMinSdkVersion = 27
 val flutterTargetSdkVersion = 36
 val flutterNdkVersion = "27.0.12077973"
-val flutterVersionCode = 2025100903
-val flutterVersionName = "4.2.39-BCH"
+val flutterVersionCode = 2025101001
+val flutterVersionName = "4.2.42-BCH"
 
 android {
     namespace = "com.mahakka"
